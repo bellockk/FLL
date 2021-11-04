6ef6c4577496894ff6f61c86ba41231f5367b00c
Kenneth E. Bellock <ken@bellock.net>
2021-11-04 18:25:04 -0400

    Capturing progress

--- Projects/test_project/main copy.py
+++ Projects/test_project/main copy.py
@@ -13,9 +13,8 @@ import logging
 
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
-from PID import PID
 
 log = logging.getLogger('FLL')
 
@@ -30,12 +29,12 @@ line_sensor = ColorSensor(Port.S3)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=124)
 
 # Calculate the light threshold. Choose values based on your measurements.
-BLACK = 5
-WHITE = 67
+BLACK = 9
+WHITE = 85
 threshold = (BLACK + WHITE) / 2
 
 # Set the drive speed at 100 millimeters per second.
-DRIVE_SPEED = 150
+DRIVE_SPEED = 50
 
 # Set the gain of the proportional line controller. This means that for every
 # percentage point of light deviating from the threshold, we set the turn
@@ -43,54 +42,42 @@ DRIVE_SPEED = 150
 
 # For example, if the light value deviates from the threshold by 10, the robot
 # steers at 10*1.2 = 12 degrees per second.
+PROPORTIONAL_GAIN = .21
 
 # Get on Left line
-TARGET_LOOP_PERIOD = 10  # ms
-pid = PID(12.0, 0.0, 0.00, setpoint=0)
 def follow_line_until(callback):
 
-    single_loop_timer = StopWatch()
-    control_loop_timer = StopWatch()
-    control_loop_count = 0
     # Start following the line endlessly.
     while True:
-        single_loop_timer.reset()
-        if control_loop_count == 0:
-            average_control_loop_period = TARGET_LOOP_PERIOD / 1000.
-            control_loop_timer.reset()
-        else:
-            average_control_loop_period = (control_loop_timer.time() / 1000 / control_loop_count)
-        control_loop_count += 1
-
+        # Calculate the deviation from the threshold.
+        deviation = line_sensor.reflection() - threshold
+    
         # Calculate the turn rate.
-        error = line_sensor.reflection() - threshold
-        control = pid(error)
-        turn_rate = control
-        if abs(control) > 350:
-            speed = 0
-        else:
-            speed = 0
+        turn_rate = -PROPORTIONAL_GAIN * deviation
     
         # Set the drive base speed and turn rate.
-        log.info('Turn Rate: %s Speed: %s Control: %s Error: %s' % (turn_rate, speed, control, error))
-
-        robot.drive(speed, turn_rate)
+        robot.drive(DRIVE_SPEED, turn_rate)
     
         # First Change lines
         if callback():
             return
     
+        # Log the drive distance
+        log.info('Distance: %s' % robot.distance())
+    
         # You can wait for a short time or do other things in this loop.
-        wait(TARGET_LOOP_PERIOD - single_loop_timer.time())
-# Effective range of speed 0-400
-# Effective range of turn 0-400
-# Error ranges from -30 - 30
+        wait(10)
 
+# while robot.distance() > -115:
+#     robot.drive(-DRIVE_SPEED, 0)
 
 # Reset the distance calculator
 robot.reset()
-follow_line_until(lambda _ = None: robot.distance() > 100)
-# wait(100)
-# Calibration of light reflection sensor
-# while True:
-#     log.info(line_sensor.reflection())
+first_change_line = False
+
+follow_line_until(lambda _ = None: robot.distance() < -900)
+robot.turn(-40)
+robot.reset()
+follow_line_until(lambda _ = None: robot.distance() < -390)
+while robot.distance() > -40:
+    robot.drive(-DRIVE_SPEED, 0)
