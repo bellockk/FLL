90e5c5c61d16319118868d998a2deddce4c45e61
Kenneth E. Bellock <ken@bellock.net>
2022-02-10 21:19:41 -0500

    Capturing updates to Robot Design Documentation

---
+++ Projects/test_project/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sshpass -p scp -rv * robot@ev3dev:test_projec
+
--- Projects/test_project/main.py
+++ Projects/test_project/main.py
@@ -1,37 +1,101 @@
 #!/usr/bin/env pybricks-micropython
 """
-Example LEGO® MINDSTORMS® EV3 Robot Educator Color Sensor Down Program
-----------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+Run 1
+test
 """
 import logging
-import datetime
 
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, GyroSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait, StopWatch
 from pybricks.robotics import DriveBase
-from PID import PID
+# from pybot.PID import PID
+# from pybot.timed_loop import timed_loop
+import pybot
+
+import config
 
 log = logging.getLogger('FLL')
+log.setLevel(logging.DEBUG)
+
+# Create a PyBot Environment
+env = pybot.Environment()
+
+# Brick
+env.ev3 = EV3Brick()
 
 # Initialize the motors.
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.A)
+env.left_motor = Motor(getattr(Port, config.LEFT_MOTOR_PORT))
+env.right_motor = Motor(getattr(Port, config.RIGHT_MOTOR_PORT))
 
 # Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
+env.center_line_sensor = ColorSensor(getattr(Port, config.CENTER_COLOR_SENSOR_PORT))
+env.left_line_sensor = ColorSensor(getattr(Port, config.LEFT_COLOR_SENSOR_PORT))
+env.right_line_sensor = ColorSensor(getattr(Port, config.RIGHT_COLOR_SENSOR_PORT))
+
+# Initialize the Gyro Sensor.
+# env.gyro = GyroSensor(getattr(Port, config.GYRO_SENSOR_PORT))
 
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=124)
+env.wheels = DriveBase(
+    env.left_motor, env.right_motor,
+    wheel_diameter=config.WHEEL_DIAMETER,
+    axle_track=config.AXLE_TRACK)
 
-while True:
-    log.info(datetime.datetime.now())
+def ramp(target, start=0, ramp_time=1000):
+    timer = StopWatch()
+    while True:
+        current_time = timer.time()
+        if current_time > ramp_time:
+            yield target
+        else:
+            yield (start + target) * (1 - (ramp_time - current_time) / ramp_time)
+
+def move_forward(env, distance):
+    rampup = ramp(-400, ramp_time=750)
+    rampdown = ramp(0, start=-400, ramp_time=750)
+    timer = StopWatch()
+    ramping_up = True
+    while True:
+        if ramping_up:
+            speed = next(rampup)
+            if speed == -400:
+                ramping_up = False
+        else:
+            speed = next(rampdown)
+            if speed == 0:
+                log.info('break')
+                break
+        log.info('Speed: %s' % speed)
+        env.wheels.drive(speed, 0)
+        yield
+
+
+env.queue.append(move_forward(env, 10))
+env.run()
+
+# rampup = ramp(-400, ramp_time=750)
+# rampdown = ramp(0, start=-400, ramp_time=750)
+# state = 'RAMPUP'
+# for frame in timed_loop(config.LOOP_FREQUENCY):
+#     if state == 'RAMPUP':
+#         speed = next(rampup)
+#         if speed == -400:
+#             state = 'RAMPDOWN'
+#     elif state == 'RAMPDOWN':
+#         speed = next(rampdown)
+#         if speed == 0:
+#             break
+#     log.info('Speed: %s' % speed)
+#     robot.drive(speed, 0)
+#     # log.info('Frame: %s' % frame)
+#     # log.info('Time: %s' % current_time)
+#     # log.info('Center Line Sensor Reflection: %s' % (center_line_sensor.reflection()))
+#     # log.info('Left Line Sensor Reflection:   %s' % (left_line_sensor.reflection()))
+#     # log.info('Right Line Sensor Reflection:  %s' % (right_line_sensor.reflection()))
+#     # log.info('Gyro:  %s' % (gyro.angle()))
+
 
 # # Calculate the light threshold. Choose values based on your measurements.
 # BLACK = 5
