8d1326880dd1bf6a887b10beb16708804636af23
Kenneth E. Bellock <ken@bellock.net>
2023-11-07 11:36:56 -0500

    Capturing updates

--- Projects/test_project/launch.sh
+++ Projects/test_project/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sshpass -p scp -rv * robot@ev3dev:test_projec
-
+# sshpass -p scp -rv * robot@ev3dev:test_project
+sshpass -p maker rsync -av --delete "$PWD" robot@ev3dev:.
+sshpass -p maker ssh robot@ev3dev "brickrun --directory \"/home/robot/${PWD##*/}\" \"/home/robot/${PWD##*/}/$1\""
